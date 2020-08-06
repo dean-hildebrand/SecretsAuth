487337c1cfe08b1221a3c549b79554a238d9d084
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-08-06 10:01:29 -0400

    Create post route for register and create new user then render the secrets page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const ejs = require('ejs')
 
 const app = express()
@@ -9,6 +10,15 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
+
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = new mongoose.model("User", userSchema)
 
 
 app.get('/', function(req, res){
@@ -21,6 +31,20 @@ app.get('/login', function(req, res){
 
 app.get('/register', function(req, res){
   res.render("register")
+})
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+  newUser.save(function(err){
+    if (!err) {
+      res.render("secrets")
+    } else {
+      console.log(err);
+    }
+  })
 })
 
 
