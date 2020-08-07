634bed41e5a00542f031d03288cf2af61b3a2559
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-08-07 16:05:32 -0400

    post route to add secrets, foreach in ejs file to display all secrets

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const ejs = require('ejs')
-const session = require('express-sesson')
+const session = require('express-session')
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
-
 
 const app = express()
 
@@ -16,21 +15,32 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set("useCreateIndex", true)
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  secret: String
 })
 
-
-// encrypts the password field in database
-// grabs environement variable with process.env
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] })
-
+userSchema.plugin(passportLocalMongoose)
 
 const User = new mongoose.model("User", userSchema)
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/', function(req, res){
   res.render("home")
@@ -44,14 +54,78 @@ app.get('/register', function(req, res){
   res.render("register")
 })
 
+app.get('/secrets', function(req, res){
+  User.find({ "secrets": {$ne: null}}, function(err, foundUsers){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers})
+      }
+    }
+  })
+})
+
+app.get('/submit', function(req, res){
+  if (req.isAuthenticated()) {
+    res.render("submit")
+  } else {
+    res.redirect('/login')
+  }
+})
+
+app.post('/submit', function(req, res){
+  const submittedSecret = req.body.secret
+
+  User.findById(req.user.id, function(err, foundUser){
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secre = submittedSecret
+        foundUser.save(function(){
+          res.redirect("/secrets")
+        })
+      }
+    }
+  })
+})
+
+app.get('/logout', function(req, res){
+  req.logout()
+  res.redirect('/')
+})
+
 // Registers a new user and adds them to the database
 app.post("/register", function(req, res){
+
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register")
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("secrets")
+      })
+    }
+  })
 
 })
 
 // Checking if user is already in the database
 app.post('/login', function(req, res){
-
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  })
+  req.login(user, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate('local')
+      res.redirect('/secrets')
+    }
+  })
 })
 
 
